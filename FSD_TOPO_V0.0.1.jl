54635292c6edf7d63dbb564ed3d5384e7659a0eb
r-acad <81679447+r-acad@users.noreply.github.com>
2021-03-31 13:37:25 +0200

    Update FSD_TOPO_V0.0.1.jl

--- FSD_TOPO_V0.0.1.jl
+++ FSD_TOPO_V0.0.1.jl
@@ -31,6 +31,33 @@ end
 # ╔═╡ 13b32a20-9206-11eb-3af7-0feea278594c
 TableOfContents(aside=true)
 
+# ╔═╡ d88f8062-920f-11eb-3f57-63a28f681c3a
+md"""
+### INITIALIZE MODEL
+"""
+
+# ╔═╡ f60365a0-920d-11eb-336a-bf5953215934
+begin
+
+nelx = 9 ; nely = 3  #mesh size
+
+F = zeros(2*(nelx+1)*(nely+1))	# Initialize external forces vector
+F[2] = -1.0	   # Applied external force
+		
+U = zeros(2*(nelx+1)*(nely+1))	# Initialize global displacements
+
+t = 1.0 .* ones(1:nelx,1:nely) # Initialize thickness distribution
+			
+fixeddofs = [Vector(1:2:2*(nely+1)) ; [2*(nelx+1)*(nely+1)] ]
+alldofs = Vector(1:2*(nelx+1)*(nely+1))
+freedofs = setdiff(alldofs,fixeddofs)			
+end;
+
+# ╔═╡ d108d820-920d-11eb-2eee-bb6470fb4a56
+md"""
+### AUXILIARY FUNCTIONS
+"""
+
 # ╔═╡ cd707ee0-91fc-11eb-134c-2fdd7aa2a50c
 function KE_CQUAD4()
 # Element stiffness matrix reverse-engineered from NASTRAN with E = 1, t = 1, nu=.03
@@ -48,6 +75,41 @@ KE = [ 	[ 1  D  A -G   B -D  C  G];
 		]'	
 end	
 	
+
+# ╔═╡ 2c768930-9210-11eb-26f8-0dc24f22afaf
+begin
+
+K = zeros(2*(nely+1)*(nelx+1), 2*(nely+1)*(nelx+1))	# Initialize global stiffness matrix
+
+KE = KE_CQUAD4()
+	
+for x = 1:nelx, y = 1:nely	
+		
+	# Node numbers, starting at top left corner and growing in columns going down as per 99 lines of code
+	n1 = (nely+1)*(x-1) + y ; n2 = n1 + 1
+	n3 = (nely+1)*x + y     ; n4 = n3 + 1
+		
+	# Numbering of node DoFs in global stiffness matrix; m_nnode_dof
+	m12 = 2*n1   ; m11 = m12 -1
+	m22 = 2*n2   ; m21 = m22 -1
+	m32 = 2*n3   ; m31 = m32 -1	
+	m42 = 2*n4   ; m41 = m42 -1
+
+	edof = [m11, m12, m21, m22, m31, m32, m41, m42 ]
+			
+	K[edof, edof] += t[x,y] * KE			
+
+		
+end # for
+
+	U[freedofs] = K[freedofs,freedofs] \ F[freedofs]
+	
+	
+	
+end
+
+# ╔═╡ 4aba92de-9212-11eb-2089-073a71342bb0
+heatmap(reverse(K, dims=1), aspect_ratio = 1, c=cgrad(:roma, 10, categorical = true))
 
 # ╔═╡ c652e5c0-9207-11eb-3310-ddef16cdb1ac
 heatmap(reverse(KE_CQUAD4(), dims=1), aspect_ratio = 1, c=cgrad(:roma, 10, categorical = true))
@@ -81,8 +143,13 @@ heatmap(reverse(SU_CQUAD4(), dims=1), aspect_ratio = 1, c=cgrad(:roma, 10, categ
 # ╔═╡ Cell order:
 # ╟─13b32a20-9206-11eb-3af7-0feea278594c
 # ╟─fc7e00a0-9205-11eb-039c-23469b96de19
+# ╟─d88f8062-920f-11eb-3f57-63a28f681c3a
+# ╠═f60365a0-920d-11eb-336a-bf5953215934
+# ╠═2c768930-9210-11eb-26f8-0dc24f22afaf
+# ╠═4aba92de-9212-11eb-2089-073a71342bb0
+# ╟─d108d820-920d-11eb-2eee-bb6470fb4a56
 # ╟─cd707ee0-91fc-11eb-134c-2fdd7aa2a50c
-# ╠═c652e5c0-9207-11eb-3310-ddef16cdb1ac
-# ╠═c1711000-920b-11eb-14ba-eb5ce08f3941
+# ╟─c652e5c0-9207-11eb-3310-ddef16cdb1ac
+# ╟─c1711000-920b-11eb-14ba-eb5ce08f3941
 # ╟─d36a8b50-920c-11eb-2667-1735bcf374de
 # ╟─c58a7360-920c-11eb-2a15-bda7ed075812
