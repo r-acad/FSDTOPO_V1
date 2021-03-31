737d086aa8004f8a91c6c3b148311926fb867d57
r-acad <81679447+r-acad@users.noreply.github.com>
2021-03-31 21:32:39 +0200

    Update FSD_TOPO_V0.0.1.jl
    First successful run of FSDTOPO

--- FSD_TOPO_V0.0.1.jl
+++ FSD_TOPO_V0.0.1.jl
@@ -39,7 +39,8 @@ md"""
 # ╔═╡ f60365a0-920d-11eb-336a-bf5953215934
 begin
 
-nelx = 60 ; nely = 20  #mesh size
+scale = 2
+nelx = 60*scale ; nely = 20*scale  #mesh size
 
 nDoF = 	2*(nely+1)*(nelx+1)  # Total number of degrees of freedom
 	
@@ -48,18 +49,12 @@ F[2] = -1.0	   # Applied external force
 		
 U = zeros(nDoF)	# Initialize global displacements
 
-t = 1.0 .* ones(1:nely,1:nelx) # Initialize thickness distribution
-			
+th = 1.0 .* ones(1:nely,1:nelx) # Initialize thickness distribution		
+	
 fixeddofs = [Vector(1:2:2*(nely+1)) ; [nDoF] ]
 alldofs   = Vector(1:nDoF)
 freedofs  = setdiff(alldofs,fixeddofs)			
 end;
-
-# ╔═╡ 68497400-922d-11eb-3c9f-237e819289df
-heatmap(reverse([1 2 3 3 3 3  ; 4 5 5 5 5 5  ; 7 8 8 8 8 8 ], dims=1), aspect_ratio = 1, c=cgrad(:roma, 10, categorical = true)) 
-
-# ╔═╡ c87645b0-922d-11eb-374d-bda8ab4f5254
-size([1 2 3 3 3 3  ; 4 5 5 5 5 5  ; 7 8 8 8 8 8 ])
 
 # ╔═╡ d108d820-920d-11eb-2eee-bb6470fb4a56
 md"""
@@ -84,16 +79,35 @@ KE = [ 	[ 1  D  A -G   B -D  C  G];
 end	
 	
 
+# ╔═╡ 944f5b10-9236-11eb-05c2-45824bc3b532
+begin
+
+function NODAL_DISPLACEMENTS(th)
+K = zeros(nDoF, nDoF)	# Initialize global stiffness matrix
+KE = KE_CQUAD4()
+	
+for y = 1:nely, x = 1:nelx			
+# Node numbers, starting at top left corner and growing in columns going down as per in 99 lines of code	
+	n1 = (nely+1)*(x-1)+y ;	n2 = (nely+1)* x +y		
+	edof = [2*n1-1; 2*n1; 2*n2-1; 2*n2; 2*n2+1;2*n2+2;2*n1+1; 2*n1+2]				
+	K[edof, edof] += th[y,x] * KE					
+end # for
+
+	# Solve linear system and get global displacements vector U
+	U[freedofs] = K[freedofs,freedofs] \ F[freedofs]
+
+end # function
+	
+end
+
 # ╔═╡ c652e5c0-9207-11eb-3310-ddef16cdb1ac
-heatmap(reverse(KE_CQUAD4(), dims=1), aspect_ratio = 1, c=cgrad(:roma, 10, categorical = true))
+#heatmap(reverse(KE_CQUAD4(), dims=1), aspect_ratio = 1, c=cgrad(:roma, 10, categorical = true))
 
 # ╔═╡ c1711000-920b-11eb-14ba-eb5ce08f3941
 function SU_CQUAD4()
 # Matrix relating cartesian stress components (sxx, syy, sxy) with nodal displacement in CQUAD4 element, reverse-engineered from NASTRAN with E = 1, t = 1, nu=.03
 		
-A = -1.209677E+00  
-B = -3.629032E-01 
-C = -4.233871E-01   	
+A = -1.209677E+00 ; B = -3.629032E-01 ; C = -4.233871E-01   	
 
 KE = [ 	[ A  B  C ];
 		[ B  A  C ];
@@ -110,73 +124,85 @@ end
 # ╔═╡ 2c768930-9210-11eb-26f8-0dc24f22afaf
 begin
 
-K = zeros(nDoF, nDoF)	# Initialize global stiffness matrix
-
-KE = KE_CQUAD4()
-	
-for y = 1:nely, x = 1:nelx			
-	# Node numbers, starting at top left corner and growing in columns going down as per in 99 lines of code
-	
-	n1 = (nely+1)*(x-1)+y
-	n2 = (nely+1)* x +y		
-	edof = [2*n1-1; 2*n1; 2*n2-1; 2*n2; 2*n2+1;2*n2+2;2*n1+1; 2*n1+2]			
-	
-	K[edof, edof] += t[y,x] * KE					
-end # for
-
-	# Solve linear system and get global displacements vector U
-	U[freedofs] = K[freedofs,freedofs] \ F[freedofs]
-	
-
+function INTERNAL_LOADS()
 #######################	
 	
-S = zeros(1:nely,1:nelx)  # Initialize matrix containing field results (typically a stress component)
+S = zeros(1:nely,1:nelx)  # Initialize matrix containing field results (typically a stress component of function)
 SUe = SU_CQUAD4() # Matrix that relates element stresses to nodal displacements
 		
 for y = 1:nely, x = 1:nelx			
 	# Node numbers, starting at top left corner and growing in columns going down as per in 99 lines of code		
-	n1 = (nely+1)*(x-1)+y
-	n2 = (nely+1)* x +y	
+	n1 = (nely+1)*(x-1)+y;	n2 = (nely+1)* x +y	
 	Ue = U[[2*n1-1;2*n1; 2*n2-1;2*n2; 2*n2+1;2*n2+2; 2*n1+1;2*n1+2],1]
 		
-	Te = SUe * Ue  # Element stress tensor in x, y coordinates
+	Te = (SUe * Ue) .* nelx  # Element stress tensor in x, y coordinates. Scaled by mesh size
+	sxx = Te[1] ; syy = Te[2] ; sxy = Te[3]
+	
+	# Principal stresses
+	s1 = 0.5 * (sxx + syy + ((sxx - syy) ^ 2 + 4 * sxy ^ 2) ^ 0.5)
+	s2 = 0.5 * (sxx + syy - ((sxx - syy) ^ 2 + 4 * sxy ^ 2) ^ 0.5)
+	ese = (s1 ^ 2 + s2 ^ 2 - 2 * 0.3 * s1 * s2) ^ 0.5		# elastic strain energy
 		
-	S[y, x] = Ue'*KE*Ue    #Te[1]	
-	S[y, x] = Te[1]
+	#S[y, x] = Ue'*KE*Ue    # Compliance from 99 lines	
+	S[y, x] = ese
 		
 end # for	
 	
-	S	
-
-	
+	return S	
+end # function	
 	
 end;
 
-# ╔═╡ 609ce250-922d-11eb-3ae8-19b16e6cbf1e
-size(S)
+# ╔═╡ c4c9ace0-9237-11eb-1f26-334caba1248d
+begin
+
+function FSDTOPO(t, niter)	
+	
+sigma_all	= 6
+max_all_t = 5
+full_penalty_iter = 10
+max_penalty = 3	
+
+for iter in 1:niter
+
+penalty = min(1 + iter / full_penalty_iter, max_penalty)
+		
+NODAL_DISPLACEMENTS(t)
+ESE = INTERNAL_LOADS()		
+				
+t .*= ESE / sigma_all		
+		
+t = [max((max_all_t*(min(nt,max_all_t)/max_all_t)^penalty), 0.01) for nt in t]		
+		
+		
+end		
+		
+t	
+end # end function
+	
+end
+
+# ╔═╡ d007f530-9255-11eb-2329-9502dc270b0d
+newt = FSDTOPO(th, 14);
 
 # ╔═╡ 4aba92de-9212-11eb-2089-073a71342bb0
-heatmap(reverse(S, dims=1), aspect_ratio = 1, c=cgrad(:roma, 10, categorical = true))
-
-# ╔═╡ d36a8b50-920c-11eb-2667-1735bcf374de
- SU_CQUAD4()
+heatmap(reverse(newt, dims=1), aspect_ratio = 1, c=cgrad(:roma, 10, categorical = true))
 
 # ╔═╡ c58a7360-920c-11eb-2a15-bda7ed075812
-heatmap(reverse(SU_CQUAD4(), dims=1), aspect_ratio = 1, c=cgrad(:roma, 10, categorical = true))
+#heatmap(reverse(SU_CQUAD4(), dims=1), aspect_ratio = 1, c=cgrad(:roma, 10, categorical = true))
 
 # ╔═╡ Cell order:
 # ╟─13b32a20-9206-11eb-3af7-0feea278594c
 # ╟─fc7e00a0-9205-11eb-039c-23469b96de19
 # ╟─d88f8062-920f-11eb-3f57-63a28f681c3a
 # ╠═f60365a0-920d-11eb-336a-bf5953215934
-# ╠═2c768930-9210-11eb-26f8-0dc24f22afaf
-# ╠═609ce250-922d-11eb-3ae8-19b16e6cbf1e
+# ╠═d007f530-9255-11eb-2329-9502dc270b0d
+# ╠═c4c9ace0-9237-11eb-1f26-334caba1248d
 # ╠═4aba92de-9212-11eb-2089-073a71342bb0
-# ╠═68497400-922d-11eb-3c9f-237e819289df
-# ╠═c87645b0-922d-11eb-374d-bda8ab4f5254
+# ╠═944f5b10-9236-11eb-05c2-45824bc3b532
+# ╠═2c768930-9210-11eb-26f8-0dc24f22afaf
 # ╟─d108d820-920d-11eb-2eee-bb6470fb4a56
-# ╟─cd707ee0-91fc-11eb-134c-2fdd7aa2a50c
+# ╠═cd707ee0-91fc-11eb-134c-2fdd7aa2a50c
 # ╠═c652e5c0-9207-11eb-3310-ddef16cdb1ac
-# ╟─c1711000-920b-11eb-14ba-eb5ce08f3941
-# ╟─d36a8b50-920c-11eb-2667-1735bcf374de
+# ╠═c1711000-920b-11eb-14ba-eb5ce08f3941
 # ╟─c58a7360-920c-11eb-2a15-bda7ed075812
