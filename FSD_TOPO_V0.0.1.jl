d8004dbee499fcb71a125203f00dadbffaf8663e
r-acad <81679447+r-acad@users.noreply.github.com>
2021-03-31 12:39:27 +0200

    Update FSD_TOPO_V0.0.1.jl

--- FSD_TOPO_V0.0.1.jl
+++ FSD_TOPO_V0.0.1.jl
@@ -29,43 +29,60 @@ begin
 end
 
 # ╔═╡ 13b32a20-9206-11eb-3af7-0feea278594c
-PlutoUI.TableOfContents(aside=true)
+TableOfContents(aside=true)
 
 # ╔═╡ cd707ee0-91fc-11eb-134c-2fdd7aa2a50c
 function KE_CQUAD4()
 # Element stiffness matrix reverse-engineered from NASTRAN with E = 1, t = 1, nu=.03
-	
-	
-A = -5.766129E-01   
-B = -6.330645E-01  
-C =  2.096774E-01   	
-D = 3.931452E-01	
-G = 3.024194E-02	
+		
+A = -5.766129E-01; B = -6.330645E-01 ; C =  2.096774E-01 ; D = 3.931452E-01	; G = 3.024194E-02	
 
-
-KE = [ 	[1 D A -G     B -D C G];
-		[D 1 G C   -D B -G A];
-		[A G 1 -D   C -G B D];
-		[-G C -D 1   G A D B];
-		[B -D C G   1 D A -G];
-		[-D B -G A   D 1 G C];
-		[C -G B D   A G 1 -D];
-		[G A D B   -G C -D 1]		
+KE = [ 	[ 1  D  A -G   B -D  C  G];
+		[ D  1  G  C  -D  B -G  A];
+		[ A  G  1 -D   C -G  B  D];
+		[-G  C -D  1   G  A  D  B];
+		[ B -D  C  G   1  D  A -G];
+		[-D  B -G  A   D  1  G  C];
+		[ C -G  B  D   A  G  1 -D];
+		[ G  A  D  B  -G  C -D  1]		
 		]'	
-
-
 end	
 	
-
-# ╔═╡ f2946650-91fc-11eb-3f39-65d37b9dfe24
-KE_CQUAD4()
 
 # ╔═╡ c652e5c0-9207-11eb-3310-ddef16cdb1ac
 heatmap(reverse(KE_CQUAD4(), dims=1), aspect_ratio = 1, c=cgrad(:roma, 10, categorical = true))
 
+# ╔═╡ c1711000-920b-11eb-14ba-eb5ce08f3941
+function SU_CQUAD4()
+# Matrix relating cartesian stress components (sxx, syy, sxy) with nodal displacement in CQUAD4 element, reverse-engineered from NASTRAN with E = 1, t = 1, nu=.03
+		
+A = -1.209677E+00  
+B = -3.629032E-01 
+C = -4.233871E-01   	
+
+KE = [ 	[ A  B  C ];
+		[ B  A  C ];
+		[-A -B  C ];
+		[ B  A -C ];
+		[-A -B -C ];
+		[-B -A -C ];
+		[ A  B -C ];
+		[-B -A  C ];
+		]'	
+
+end	
+
+# ╔═╡ d36a8b50-920c-11eb-2667-1735bcf374de
+ SU_CQUAD4()
+
+# ╔═╡ c58a7360-920c-11eb-2a15-bda7ed075812
+heatmap(reverse(SU_CQUAD4(), dims=1), aspect_ratio = 1, c=cgrad(:roma, 10, categorical = true))
+
 # ╔═╡ Cell order:
-# ╠═13b32a20-9206-11eb-3af7-0feea278594c
-# ╠═fc7e00a0-9205-11eb-039c-23469b96de19
-# ╠═cd707ee0-91fc-11eb-134c-2fdd7aa2a50c
-# ╠═f2946650-91fc-11eb-3f39-65d37b9dfe24
+# ╟─13b32a20-9206-11eb-3af7-0feea278594c
+# ╟─fc7e00a0-9205-11eb-039c-23469b96de19
+# ╟─cd707ee0-91fc-11eb-134c-2fdd7aa2a50c
 # ╠═c652e5c0-9207-11eb-3310-ddef16cdb1ac
+# ╠═c1711000-920b-11eb-14ba-eb5ce08f3941
+# ╟─d36a8b50-920c-11eb-2667-1735bcf374de
+# ╟─c58a7360-920c-11eb-2a15-bda7ed075812
