fd262ef4e58201c94a9fd0a0af3f83f705269596
r-acad <81679447+r-acad@users.noreply.github.com>
2021-03-31 17:00:06 +0200

    Update FSD_TOPO_V0.0.1.jl

--- FSD_TOPO_V0.0.1.jl
+++ FSD_TOPO_V0.0.1.jl
@@ -39,19 +39,27 @@ md"""
 # ╔═╡ f60365a0-920d-11eb-336a-bf5953215934
 begin
 
-nelx = 9 ; nely = 3  #mesh size
+nelx = 60 ; nely = 20  #mesh size
 
-F = zeros(2*(nelx+1)*(nely+1))	# Initialize external forces vector
+nDoF = 	2*(nely+1)*(nelx+1)  # Total number of degrees of freedom
+	
+F = zeros(nDoF)	# Initialize external forces vector
 F[2] = -1.0	   # Applied external force
 		
-U = zeros(2*(nelx+1)*(nely+1))	# Initialize global displacements
+U = zeros(nDoF)	# Initialize global displacements
 
-t = 1.0 .* ones(1:nelx,1:nely) # Initialize thickness distribution
+t = 1.0 .* ones(1:nely,1:nelx) # Initialize thickness distribution
 			
-fixeddofs = [Vector(1:2:2*(nely+1)) ; [2*(nelx+1)*(nely+1)] ]
-alldofs = Vector(1:2*(nelx+1)*(nely+1))
-freedofs = setdiff(alldofs,fixeddofs)			
+fixeddofs = [Vector(1:2:2*(nely+1)) ; [nDoF] ]
+alldofs   = Vector(1:nDoF)
+freedofs  = setdiff(alldofs,fixeddofs)			
 end;
+
+# ╔═╡ 68497400-922d-11eb-3c9f-237e819289df
+heatmap(reverse([1 2 3 3 3 3  ; 4 5 5 5 5 5  ; 7 8 8 8 8 8 ], dims=1), aspect_ratio = 1, c=cgrad(:roma, 10, categorical = true)) 
+
+# ╔═╡ c87645b0-922d-11eb-374d-bda8ab4f5254
+size([1 2 3 3 3 3  ; 4 5 5 5 5 5  ; 7 8 8 8 8 8 ])
 
 # ╔═╡ d108d820-920d-11eb-2eee-bb6470fb4a56
 md"""
@@ -76,41 +84,6 @@ KE = [ 	[ 1  D  A -G   B -D  C  G];
 end	
 	
 
-# ╔═╡ 2c768930-9210-11eb-26f8-0dc24f22afaf
-begin
-
-K = zeros(2*(nely+1)*(nelx+1), 2*(nely+1)*(nelx+1))	# Initialize global stiffness matrix
-
-KE = KE_CQUAD4()
-	
-for x = 1:nelx, y = 1:nely	
-		
-	# Node numbers, starting at top left corner and growing in columns going down as per 99 lines of code
-	n1 = (nely+1)*(x-1) + y ; n2 = n1 + 1
-	n3 = (nely+1)*x + y     ; n4 = n3 + 1
-		
-	# Numbering of node DoFs in global stiffness matrix; m_nnode_dof
-	m12 = 2*n1   ; m11 = m12 -1
-	m22 = 2*n2   ; m21 = m22 -1
-	m32 = 2*n3   ; m31 = m32 -1	
-	m42 = 2*n4   ; m41 = m42 -1
-
-	edof = [m11, m12, m21, m22, m31, m32, m41, m42 ]
-			
-	K[edof, edof] += t[x,y] * KE			
-
-		
-end # for
-
-	U[freedofs] = K[freedofs,freedofs] \ F[freedofs]
-	
-	
-	
-end
-
-# ╔═╡ 4aba92de-9212-11eb-2089-073a71342bb0
-heatmap(reverse(K, dims=1), aspect_ratio = 1, c=cgrad(:roma, 10, categorical = true))
-
 # ╔═╡ c652e5c0-9207-11eb-3310-ddef16cdb1ac
 heatmap(reverse(KE_CQUAD4(), dims=1), aspect_ratio = 1, c=cgrad(:roma, 10, categorical = true))
 
@@ -134,6 +107,57 @@ KE = [ 	[ A  B  C ];
 
 end	
 
+# ╔═╡ 2c768930-9210-11eb-26f8-0dc24f22afaf
+begin
+
+K = zeros(nDoF, nDoF)	# Initialize global stiffness matrix
+
+KE = KE_CQUAD4()
+	
+for y = 1:nely, x = 1:nelx			
+	# Node numbers, starting at top left corner and growing in columns going down as per in 99 lines of code
+	
+	n1 = (nely+1)*(x-1)+y
+	n2 = (nely+1)* x +y		
+	edof = [2*n1-1; 2*n1; 2*n2-1; 2*n2; 2*n2+1;2*n2+2;2*n1+1; 2*n1+2]			
+	
+	K[edof, edof] += t[y,x] * KE					
+end # for
+
+	# Solve linear system and get global displacements vector U
+	U[freedofs] = K[freedofs,freedofs] \ F[freedofs]
+	
+
+#######################	
+	
+S = zeros(1:nely,1:nelx)  # Initialize matrix containing field results (typically a stress component)
+SUe = SU_CQUAD4() # Matrix that relates element stresses to nodal displacements
+		
+for y = 1:nely, x = 1:nelx			
+	# Node numbers, starting at top left corner and growing in columns going down as per in 99 lines of code		
+	n1 = (nely+1)*(x-1)+y
+	n2 = (nely+1)* x +y	
+	Ue = U[[2*n1-1;2*n1; 2*n2-1;2*n2; 2*n2+1;2*n2+2; 2*n1+1;2*n1+2],1]
+		
+	Te = SUe * Ue  # Element stress tensor in x, y coordinates
+		
+	S[y, x] = Ue'*KE*Ue    #Te[1]	
+	S[y, x] = Te[1]
+		
+end # for	
+	
+	S	
+
+	
+	
+end;
+
+# ╔═╡ 609ce250-922d-11eb-3ae8-19b16e6cbf1e
+size(S)
+
+# ╔═╡ 4aba92de-9212-11eb-2089-073a71342bb0
+heatmap(reverse(S, dims=1), aspect_ratio = 1, c=cgrad(:roma, 10, categorical = true))
+
 # ╔═╡ d36a8b50-920c-11eb-2667-1735bcf374de
  SU_CQUAD4()
 
@@ -146,10 +170,13 @@ heatmap(reverse(SU_CQUAD4(), dims=1), aspect_ratio = 1, c=cgrad(:roma, 10, categ
 # ╟─d88f8062-920f-11eb-3f57-63a28f681c3a
 # ╠═f60365a0-920d-11eb-336a-bf5953215934
 # ╠═2c768930-9210-11eb-26f8-0dc24f22afaf
+# ╠═609ce250-922d-11eb-3ae8-19b16e6cbf1e
 # ╠═4aba92de-9212-11eb-2089-073a71342bb0
+# ╠═68497400-922d-11eb-3c9f-237e819289df
+# ╠═c87645b0-922d-11eb-374d-bda8ab4f5254
 # ╟─d108d820-920d-11eb-2eee-bb6470fb4a56
 # ╟─cd707ee0-91fc-11eb-134c-2fdd7aa2a50c
-# ╟─c652e5c0-9207-11eb-3310-ddef16cdb1ac
+# ╠═c652e5c0-9207-11eb-3310-ddef16cdb1ac
 # ╟─c1711000-920b-11eb-14ba-eb5ce08f3941
 # ╟─d36a8b50-920c-11eb-2667-1735bcf374de
 # ╟─c58a7360-920c-11eb-2a15-bda7ed075812
